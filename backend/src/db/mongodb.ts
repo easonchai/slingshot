abe5dced7d87c121369b534aeb6e49310087b735
Eduard Besjentsev <fc.puntje@gmail.com>
2020-05-05 18:04:42 +0200

    Connect to MongoDB based on .env variables logic.

--- backend/src/db/mongodb.ts
+++ backend/src/db/mongodb.ts
@@ -1,41 +1,46 @@
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import { MongoError } from 'mongodb';
+import logger from '@shared/Logger';
+
 
 export class MongoDB {
 
-  private connectionString;
+  private useMongoDB: boolean;
+  private connectionString: string;
 
   public constructor() {
-    this.useMongoDB = process.env.MONGODB_ACTIVE || false;
+    const active = process.env.MONGODB_ACTIVE || 'false';
+    this.useMongoDB = active.toLowerCase() == 'true';
     this.connectionString = process.env.MONGODB_URI || '';
 
     if (!this.useMongoDB) {
-      console.log('set MONGODB_ACTIVE=true in /backend/env/*.env file to connect to MongoDB.');
+      logger.info('set MONGODB_ACTIVE=true in /backend/env/*.env file to connect to MongoDB');
     } else if (this.connectionString === '') {
-      console.log('Please update your MONGODB_URI to connect to MongoDB database in /backend/env/*.env file.');
-      console.log('Alternatively set MONGODB_ACTIVE=false to disable database connection.');
+      logger.info('Please update your MONGODB_URI to connect to MongoDB database in /backend/env/*.env file');
+      logger.info('Alternatively set MONGODB_ACTIVE=false to disable database connection');
     }
-
-    (<any>mongoose).Promise = global.Promise;
-    mongoose.connection.on('connected', () => {
-      console.log('Connected to mongodb');
-    });
-    mongoose.connection.on('disconnected', () => {
-      console.log('Disconnected from mongodb');
-    });
   }
 
   public connect(): Promise<mongoose.Connection> {
     return new Promise((resolve, reject) => {
-      mongoose.connect(this.connectionString, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (error: MongoError) => {
-        if (error) {
-          console.log('Error while connecting to mongodb');
-          console.log(' -> ' + error.message);
-          reject();
-        } else {
-          resolve(mongoose.connection);
+      if (!this.useMongoDB) {
+        reject('MONGODB_ACTIVE=' + process.env.MONGODB_ACTIVE);
+      } else {
+        const options = {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useUnifiedTopology: true
         }
-      });
+
+        mongoose.connect(this.connectionString, options, (error: MongoError) => {
+          if (error) {
+            reject(error.message);
+          } else {
+            logger.info('Successfully connected to MongoDB');
+            resolve(mongoose.connection);
+          }
+        });
+      }
     });
   }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,24 @@ import { MongoDB } from './db/mongodb';
 import app from '@server';
 import logger from '@shared/Logger';
 
-// Start the server
+
 const port = Number(process.env.PORT || 3030);
 
-new MongoDB().connect().then(db => {
-  app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
+new MongoDB().connect()
+  .then(db => {
+    // Set handlers for future MongoDB disconnects / reconnects.
+    db.on('connected', () => {
+      console.log('Connected to MongoDB');
+    });
+    db.on('disconnected', () => {
+      console.log('Disconnected from MongoDB');
+      // TODO: verify whether we need to re-establish the connection here or if it's automatically done.
+    });
+
+    app.listen(port, () => {
+      logger.info('Express server started on port: ' + port);
+    });
+  })
+  .catch((err) => {
+    logger.info('Failed to connect to MongoDB: ' + err);
   });
-});
