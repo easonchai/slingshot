acb67508032708f188103f8876f33e703c290a86
Eduard <fc.puntje@gmail.com>
2020-05-21 01:36:25 +0200

    UAT session 1 - case 19 - Display the meetings in the order of most recent start date first.

--- backend/src/routes/Meeting.ts
+++ backend/src/routes/Meeting.ts
@@ -24,6 +24,7 @@ router.get('/all', async (req: Request, res: Response, next: NextFunction) => {
             ]
         })
         .select('-__v')
+        .sort({ 'data.startDateTime': 1 })
         .then(documents => {
             res
                 .status(OK)
